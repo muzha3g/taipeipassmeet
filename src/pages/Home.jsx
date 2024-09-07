28bfa5584f59d9470147b9e69ee92fa4c7bb3a71
shih1999 <shihchen1999@gmail.com>
2024-09-07 21:07:34 +0800

    feat: add home request

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,35 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import ActivityCard from "../components/ActivityCard";
 
 const Home = () => {
+
+  const [eventList, setEvents] = useState([{}]);
+
+  useEffect(() => {
+    axios
+      .get("http://si8a1.asuscomm.com:8000/event_list")
+      .then((response) => {
+        console.log(response.data['event']);
+        setEvents(response.data['event']);
+      })
+      .catch((error) => {
+        console.error('Error fetching event list:', error);
+      });
+    }, []);
+
   return (
-    <main className="flex flex-column flex-wrap justify-center items-center">
-      <ActivityCard />
-      <ActivityCard />
-      <ActivityCard />
+    <main className="m-5 px-5 d-flex flex-column ">
+      eventList.map((event) => (
+        <ActivityCard 
+          key={event.event_id}
+          event_name = {event.event_name}
+          date = {event.date}
+          time = {event.time}
+          location = {event.location}
+        />
+      ))
     </main>
   );
 };
