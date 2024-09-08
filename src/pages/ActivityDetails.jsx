11ff9beed64159ab9c26cf225aa5de143b8fb147
sangy <apple2951@gmail.com>
2024-09-08 10:52:43 +0800

    補 Skeleton

--- src/pages/ActivityDetails.jsx
+++ src/pages/ActivityDetails.jsx
@@ -4,9 +4,12 @@ import RoomCard from "../components/RoomCard";
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const ActivityDetails = () => {
   const { eid } = useParams();
+  const [loading, setLoading] = useState(true);
 
   const [event, setEvent] = useState([]);
   const [roomList, setRooms] = useState([{}]);
@@ -17,6 +20,7 @@ const ActivityDetails = () => {
       .then((response) => {
         // console.log(response.data);
         setEvent(response.data);
+        setLoading(false);
       })
       .catch((error) => {
         console.error("Error fetching event list:", error);
@@ -26,6 +30,7 @@ const ActivityDetails = () => {
       .then((response) => {
         // console.log(response.data);
         setRooms(response.data);
+        setLoading(false);
       })
       .catch((error) => {
         console.error("Error fetching event list:", error);
@@ -34,25 +39,34 @@ const ActivityDetails = () => {
   return (
     <>
       <main className="flex flex-col justify-center items-center">
-        <ActivityCard
-          key={event.event_id}
-          id={event.event_id}
-          event_name={event.event_name}
-          date={event.date}
-          time={event.time}
-          location={event.location}
-          img_url={event.img_url}
-        />
-        {roomList.map((room) => (
-          <RoomCard
-            key={room.roomID}
-            eid={eid}
-            rid={room.roomID}
-            name={room.roomName}
-            date={room.date}
-            time={room.time}
+        {loading ? (
+          <Skeleton />
+        ) : (
+          <ActivityCard
+            key={event.event_id}
+            id={event.event_id}
+            event_name={event.event_name}
+            date={event.date}
+            time={event.time}
+            location={event.location}
+            img_url={event.img_url}
           />
-        ))}
+        )}
+
+        {loading ? (
+          <Skeleton />
+        ) : (
+          roomList.map((room) => (
+            <RoomCard
+              key={room.roomID}
+              eid={eid}
+              rid={room.roomID}
+              name={room.roomName}
+              date={room.date}
+              time={room.time}
+            />
+          ))
+        )}
       </main>
     </>
   );
